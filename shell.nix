4e16d0ca21a124087d3f4317720bc38356c67493
jimurrito <jimurrito@gmail.com>
2024-08-14 05:20:37 +0000

    Updated shell.nix config

--- shell.nix
+++ shell.nix
@@ -1,20 +1,30 @@
-with import <nixpkgs> {};
+# Virtual environment for Jekyll blog development
+#
+# Ensure to run
+# - `bundle lock` => creates Gemfile.lock
+# - `bundix` => creates `gemset.nix`
+#
 
-let jekix-env = bundlerEnv rec {
-  name = "jekix-${version}";
-  version = "1.0.0";
 
-  inherit ruby;
-  gemfile = ./Gemfile;
-  lockfile = ./Gemfile.lock;
-  gemset = ./gemset.nix;
-};
+with import <nixpkgs> { };
+
+let
+  jekix-env = bundlerEnv rec {
+    name = "jekix";
+
+    inherit ruby;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
 
 in
 stdenv.mkDerivation {
   name = "jekix-env";
   buildInputs = [ jekyll bundler ];
   shellHook = ''
-
+    bundle install
+    bundle exec jekyll serve
+    echo && echo "Run 'bundle exec jekyll serve' to start the server again!"
   '';
 }
